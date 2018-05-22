1d44e82eefdd9aa9268376d000a1c38279cd3118
byronicone <byron.e.barnes@gmail.com>
2018-05-22 13:45:39 -0400

    Board is loading correctly for variable sizes with default player symbols / number of players

--- game.js
+++ game.js
@@ -1,45 +1,55 @@
-var exports = module.exports = {};
+var game = {};
 
 var HORIZONTAL = 1;
 var VERTICAL =  2;
 var FORWARD_DIAG = 3;
 var BACKWARD_DIAG = 2;
 
+var DEFAULT_SIZE = 3;
 
 
-loadGame = function(boardSize, player2Symbol, player1Symbol){
-
-
-  for(let row=0;row<boardSize;row++){
-    addRowToBoard(game, row);
+game.load = function(boardSize){
+  if(!(boardSize > 0)){
+    boardSize = DEFAULT_SIZE;
   }
 
+  let board = loadBoard(boardSize);
+  fillBoard(board);
 
+  let playerSymbols = loadPlayerSymbols();
+
+  let game = {
+    'board': board,
+    'size' : boardSize,
+    'playerSymbols': playerSymbols,
+  };
 
   return game;
 }
 
-loadPlayerSymbols = function(game, playerSymbols){
-  for(let symbol of playerSymbols){
-     eval(`game.player${i} = playerSymbols[i]`);
+loadPlayerSymbols = function(symbol1, symbol2){
+  let symbols = { 'player1': symbol1 || 'X', 'player2': symbol2 || 'Y'};
+  return symbols;
+}
+
+loadBoard = function(boardSize){
+  return new Array(boardSize);
+}
+
+fillBoard = function(board){
+  let boardSize = board.length;
+  for(let rowNumber=0;rowNumber<boardSize;rowNumber++){
+    let boardRow = getBoardRow(boardSize, rowNumber);
+    board[rowNumber] = boardRow;
   }
 }
 
-loadBoard = function(game, boardSize){
-  boardSize = boardSize || 3;
-  game.board = new Array(boardSize);
-  return game;
-}
-
-addRowToBoard = function(game, row){
-  let size = getBoardSize(game);
-  for( let cellNumber = (row*3)+1; cellNumber < size*(row+1)){
-    game.board[row].push(cellNumber);
+getBoardRow = function(boardSize, rowNumber){
+  let row = [];
+  for( let cellNumber = (rowNumber*boardSize)+1; cellNumber <= boardSize*(rowNumber+1); cellNumber++){
+    row.push(cellNumber);
   }
-}
-
-getBoardSize = function(game){
-  return game.board.length;
+  return row;
 }
 
 printBoard =function(game){
@@ -69,7 +79,7 @@ playTurn = function(game,num,playerSymbol){
 
   game.board[coordinates[0]][coordinates[1]] = playerSymbol;
 
-  eval(`game.${playerSymbol}.add(num)`;
+  eval(`game.${playerSymbol}.add(num)`);
 
   return game;
 }
@@ -82,9 +92,9 @@ getCoordinatesFromCellNumber = function(board, number){
 
 checkForVictory = function(game){
   //Horizontal victory = diff of 1.
-  //Vertical victory = diff of getBoardSize(game);
-  //First diagonal victory =  diff of board size + 1.  all columns, rows different
-  //second diagonal victory = diff of board size - 1.  all columns, rows different
+  //Vertical victory = diff of board size
+  //First diagonal victory =  diff of board size + 1;
+  //second diagonal victory = diff of board size - 1;
   //check player 1's set.
     //if victorious, return.
   //check player 2's set.
@@ -102,5 +112,11 @@ checkPlayerSetForVictory = function(playerSet){
   //return isVictory
 }
 
-getDiffsInPlayerSet = function(
+// getDiffsInPlayerSet = function(
+//
 
+for(prop in game) {
+   if(game.hasOwnProperty(prop)) {
+     module.exports[prop] = game[prop];
+   }
+}
